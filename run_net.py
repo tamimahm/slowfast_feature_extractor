2d072abfb2d2f40c6de2ffc6fbbfc338813b6d0c
Tamim Ahmed <tamimahm@usc.edu>
2025-03-05 01:51:01 -0500

    Update run_net.py

--- run_net.py
+++ run_net.py
@@ -4,7 +4,7 @@
 """Wrapper to train and test a video classification model."""
 from slowfast.utils.misc import launch_job
 from slowfast.utils.parser import parse_args
-from configs.custom_config import load_config
+from slowfast.config.defaults import get_cfg  # Import get_cfg directly
 
 from test_net import test
 
@@ -14,7 +14,8 @@ def main():
     Main function to spawn the train and test process.
     """
     args = parse_args()
-    cfg = load_config(args)
+    cfg = get_cfg()  # Use get_cfg directly
+    cfg.merge_from_file(args.cfg_files[0])  # Load the config file
 
     # Perform multi-clip testing.
     if cfg.TEST.ENABLE:
